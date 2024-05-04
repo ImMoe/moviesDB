ee0b6dcd38fa2071e9b24e2db7074114c58607df
Mohamed Ali <immoe98@gmail.com>
2024-05-04 21:20:23 +0200

    Display text when there is no filtered movies

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,4 +1,4 @@
-import { SimpleGrid } from '@chakra-ui/react';
+import { SimpleGrid, Text } from '@chakra-ui/react';
 import MovieItem, { Movie } from './MovieItem';
 import MovieSkeleton from './MovieSkeleton';
 
@@ -13,21 +13,27 @@ const MovieList = ({ data: movies, error, isLoading }: Props) => {
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
   if (error) return <h2>{error}</h2>;
   return (
-    <SimpleGrid
-      columns={{
-        base: 1,
-        md: 2,
-        lg: 3,
-        xl: 4,
-      }}
-      spacing={8}
-    >
-      {isLoading &&
-        skeletons.map((skeleton) => <MovieSkeleton key={skeleton} />)}
-      {movies.map((movie) => (
-        <MovieItem key={movie.id} movie={movie} />
-      ))}
-    </SimpleGrid>
+    <>
+      <Text paddingY={4}>Showing {movies.length} movies</Text>
+      <SimpleGrid
+        columns={{
+          base: 1,
+          md: 2,
+          lg: 3,
+          xl: 4,
+        }}
+        spacing={8}
+      >
+        {movies.length === 0 && (
+          <Text>There is no movies in this category. </Text>
+        )}
+        {isLoading &&
+          skeletons.map((skeleton) => <MovieSkeleton key={skeleton} />)}
+        {movies.map((movie) => (
+          <MovieItem key={movie.id} movie={movie} />
+        ))}
+      </SimpleGrid>
+    </>
   );
 };
 
