3feb62849412c8490f9d022cb66d8a71df7cb288
Mohamed Ali <immoe98@gmail.com>
2024-05-06 00:52:08 +0200

    Move API KEY to .env file for security

--- src/services/api-service.ts
+++ src/services/api-service.ts
@@ -4,7 +4,6 @@ export default axios.create({
   baseURL: 'https://api.themoviedb.org/3',
   headers: {
     'Content-Type': 'application/json',
-    Authorization:
-      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYTRlMzQ0MmIwMTE5MDdiNjA2MDZlNDg4NTMwZmQ0OCIsInN1YiI6IjVlYjJiNDM1ZWRlYjQzMDAxY2NiMWM3ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.umF-oa12OCyM1O0j4wXAWMLrv4NHnjr2fLyN2FjoyF4',
+    Authorization: `Bearer ${import.meta.env.VITE_SOME_KEY}`,
   },
 });
